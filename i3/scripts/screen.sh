a415b4438a75280f1a3e86f068c3031f1b8bbfdb
Janos <janoslee@gmail.com>
2022-04-04 22:41:29 +0100

    -fixed the arrow in i3. New script to dual monitor

---
+++ i3/scripts/screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 3840x465 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --scale 0.5x0.5
